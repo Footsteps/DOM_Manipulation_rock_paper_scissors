13117d60f26c4ea834dfcbb048e046546eb3529c
Angela Schumacher <angelaschumacher@web.de>
2024-11-05 21:32:22 +0100

    resetGame, showResults and eventListeners added. done!

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 
 //variables
-let playerScore = 0;
+let humanScore = 0;
 let computerScore = 0;
 
-const playerScoreSpanElement = document.getElementById("player-score");
+const humanScoreSpanElement = document.getElementById("player-score");
 const computerScoreSpanElement = document.getElementById("computer-score");
 const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+//event listeners
+rockBtn.addEventListener("click", function () {
+  showResults("Stein");
+})
+
+paperBtn.addEventListener("click", function() {
+  showResults("Papier");
+})
+
+scissorsBtn.addEventListener("click", function() {
+  showResults("Schere");
+})
+
+resetGameBtn.addEventListener("click", resetGame);
 
 //generate computer choice
 function getRandomComputerResult() {
-  const options = ["Rock", "Paper", "Scissors"];
+  const options = ["Stein", "Papier", "Schere"];
   return options[Math.floor(Math.random() * options.length)];
 }
 
 //human wins
 function hasHumanWonRound(human, computer) {
   if (
-    (human === "Rock" && computer === "Scissors") ||
-    (human === "Scissors" && computer === "Paper") ||
-    (human === "Paper" && computer === "Rock")
+    (human === "Stein" && computer === "Schere") ||
+    (human === "Schere" && computer === "Papier") ||
+    (human === "Papier" && computer === "Stein")
   ) {
     return true;
   } else {
@@ -27,24 +48,41 @@ function hasHumanWonRound(human, computer) {
 }
 
 //round results
-function getRoundResult(userOption) {
+function getRoundResults(humanOption) {
     const computerResult = getRandomComputerResult();
-    if (hasHumanWonRound(userOption, computerResult)) {
-        playerScore += 1;
-        return `Human wins! ${userOption} beats ${computerResult}`;
-    } else if (userOption == computerResult) {
-        return `It's a tie! Both chose ${userOption}`
+    if (hasHumanWonRound(humanOption, computerResult)) {
+        humanScore++;
+        return `Mensch gewinnt! ${humanOption} schlägt ${computerResult}`;
+    } else if (humanOption == computerResult) {
+        return `Gleichstand! Ihr habt beide ${humanOption} gewählt.`
     } else {
-        computerScore += 1;
-        return `Computer wins! ${computerResult} beats ${userOption}`; 
+        computerScore++;
+        return `Computer gewinnt! ${computerResult} schlägt ${humanOption}`; 
     }
 }
 
 //game results 
-function showResults(userOption) {
-    roundResultsMsg.innerText = getRoundResults(userOption);
+function showResults(humanOption) {
+    roundResultsMsg.innerText = getRoundResults(humanOption);
     computerScoreSpanElement.innerText = computerScore;
-    playerScoreSpanElement.innerText = playerScore;
+    humanScoreSpanElement.innerText = humanScore;
+
+    if(computerScore === 3 || humanScore === 3) {
+      computerScore === 3 ? winnerMsgElement.innerText = "Computer hat die Runde gewonnen!" : winnerMsgElement.innerText = "Mensch hat die Runde gewonnen!";
+      resetGameBtn.style.display = "block";
+      optionsContainer.style.display = "none";
+    }
 }
 
-showResults("Rock");
+//reset game
+function resetGame() {
+humanScore = 0;
+computerScore = 0;
+humanScoreSpanElement.innerText = humanScore;
+computerScoreSpanElement.innerText = computerScore;
+resetGameBtn.style.display = "none";
+optionsContainer.style.display = "block";
+winnerMsgElement.innerText = "";
+roundResultsMsg.innerText = "";
+}
+
